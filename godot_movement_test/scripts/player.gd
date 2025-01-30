3c7b5d49178210b94ea8d409a7376936e09b9e46
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2025-01-30 13:16:40 +0100

    added double jump still fixing ledge jumping

--- godot_movement_test/scripts/player.gd
+++ godot_movement_test/scripts/player.gd
@@ -3,17 +3,27 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
-
+var jump = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.2
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():	
 		velocity.y = JUMP_VELOCITY
-
+		jump += 1
+	#allows double jump
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jump < 1:	
+		velocity.y = JUMP_VELOCITY
+		jump += 1
+		#fix this or smthing so it lets you jump even ater you kinda left the ground
+	#reset double jump
+	if is_on_floor() and jump != 0:
+		print("ass")
+		jump = 0
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -22,6 +32,6 @@ func _physics_process(delta: float) -> void:
 			velocity.x += direction * 20
 			print(direction)
 	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		velocity.x = move_toward(velocity.x, 0, 15)
 
 	move_and_slide()
