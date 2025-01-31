70e85dd4d8479e86432a7ab13902113943010f98
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2025-01-31 13:13:26 +0100

    jump buffering added i can die semi happy

--- godot_movement_test/scripts/killzone.gd
+++ godot_movement_test/scripts/killzone.gd
@@ -3,12 +3,11 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("ded")
 	timer.start()
 	
 	
 	
-
+	
 
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
--- godot_movement_test/scripts/player.gd
+++ godot_movement_test/scripts/player.gd
@@ -4,24 +4,35 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
 var jump = 0
+var jump_buffer:bool = false
+var jump_buffer_time:float = .1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 1.2
+	else:
+		#jumps you if you hit the jump before hit the gound
+		if jump_buffer:
+			jumping()
+			jump_buffer = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():	
-		velocity.y = JUMP_VELOCITY
-		jump += 1
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():	
+			jumping()
+		else:
+			#sets the buffer for jumping
+			jump_buffer = true
+			get_tree().create_timer(jump_buffer_time).timeout.connect(on_jump_buffer_timeout)
+		
 	#allows double jump
 	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jump < 1:	
-		velocity.y = JUMP_VELOCITY
-		jump += 1
+		jumping()
 		#fix this or smthing so it lets you jump even ater you kinda left the ground
+		
 	#reset double jump
 	if is_on_floor() and jump != 0:
-		print("ass")
 		jump = 0
 		
 	# Get the input direction and handle the movement/deceleration.
@@ -35,3 +46,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, 15)
 
 	move_and_slide()
+	#do reset buttons here i dont have tme
+	#if InputEventAction.:
+		#reset()
+#makes ya jump
+func jumping() -> void:
+	velocity.y = JUMP_VELOCITY
+	jump += 1
+func on_jump_buffer_timeout()->void:
+	jump_buffer = false
+	
+func reset():
+	get_tree().reload_current_scene()
