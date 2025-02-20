ba3595687e548541acf1187560d04af1653615ab
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2025-02-20 13:16:04 +0100

    i am loosing control

--- godot_movement_test/scripts/killzone.gd
+++ godot_movement_test/scripts/killzone.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 
 func _on_body_entered(body):
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	body.velocity.x = 0
 	timer.start()
 	
 	
@@ -10,4 +13,5 @@ func _on_body_entered(body):
 	
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- godot_movement_test/scripts/player.gd
+++ godot_movement_test/scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-var SPEED = 200.0
+var SPEED = 160.0
 var JUMP_VELOCITY = -350.0
 var jump = 0
 var jump_buffer:bool = false
 var jump_buffer_time:float = .1
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,7 +19,7 @@ func _physics_process(delta: float) -> void:
 			jump_buffer = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():	
 			jumping()
 		else:
@@ -27,23 +28,56 @@ func _physics_process(delta: float) -> void:
 			get_tree().create_timer(jump_buffer_time).timeout.connect(on_jump_buffer_timeout)
 		
 	#allows double jump
-	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jump < 1:	
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and jump < 1:	
 		jumping()
 		
 	#reset double jump
 	if is_on_floor() and jump != 0:
 		jump = 0
-		
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction < 0:
+		animated_sprite.flip_h = true
+	elif direction > 0:
+		animated_sprite.flip_h = false
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+			if Input.is_action_pressed("crouch"):
+				animated_sprite.play("crouch")
+		else:
+			if Input.is_action_pressed("crouch"):
+				animated_sprite.play("crouch")
+			else:
+				animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+		
+		
 	if direction:
 		if velocity.x < SPEED && velocity.x > -SPEED:
-			velocity.x += direction * 20
-			print(direction)
+			if Input.is_action_pressed("crouch") && direction > 0:
+				velocity.x = move_toward(velocity.x, SPEED/2, 5)
+			elif Input.is_action_pressed("crouch") && direction < 0:
+				velocity.x = move_toward(velocity.x, -SPEED/2, 5)
+			else:
+				velocity.x += direction * 15
+				if velocity.x > SPEED:
+					velocity.x = SPEED 
+				elif velocity.x < -SPEED:
+					velocity.x = -SPEED 
+					
+				print(velocity.x)
+				print(direction)
 	else:
+		direction = 0
 		velocity.x = move_toward(velocity.x, 0, 15)
-
 	move_and_slide()
 	#do reset buttons here i dont have tme
 	#if InputEventAction.:
