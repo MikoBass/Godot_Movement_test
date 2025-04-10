a6a3d9d7233a54c0a661510205ffa63afef61fb4
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2025-04-10 11:45:21 +0200

    i made it i am a god

--- godot_movement_test/scripts/game_manager.gd
+++ godot_movement_test/scripts/game_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 @onready var score_label: Label = %ScoreLabel
+@onready var gui: CanvasLayer = %GUI
+
 
 var score = 0
 
 func add_point():
 	score += 1
-	score_label.text = "Score: "+str(score)
+	gui.score.text = "Score: "+str(score)
